d8120fe31c3308371669833ed5252311ef1a3f3a
shilpi75 <shilpi.c75@gmail.com>
2017-03-26 05:33:53 +0530

    Image gallery

--- app/src/main/java/clashacks/commandoengineer/clashhackspic/LibraryAdapter.java
+++ app/src/main/java/clashacks/commandoengineer/clashhackspic/LibraryAdapter.java
@@ -1,13 +1,19 @@
 package clashacks.commandoengineer.clashhackspic;
 
 import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+
+import java.io.File;
+import java.util.List;
 
 /**
  * Created by shilpi on 26/3/17.
@@ -16,17 +22,19 @@ import com.bumptech.glide.Glide;
 public class LibraryAdapter extends RecyclerView.Adapter<LibraryAdapter.ViewHolder> {
 
     Context context;
+    List<String> mImageUrls;
 
-    public LibraryAdapter(Context context)
+    public LibraryAdapter(Context context, List<String> imageUrls)
     {
         this.context = context;
+        mImageUrls = imageUrls;
+
     }
 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         ImageView mImageView;
-
         public ViewHolder(View itemView) {
             super(itemView);
             mImageView = (ImageView) itemView.findViewById(R.id.imageView);
@@ -43,14 +51,15 @@ public class LibraryAdapter extends RecyclerView.Adapter<LibraryAdapter.ViewHold
     @Override
     public void onBindViewHolder(LibraryAdapter.ViewHolder holder, int position) {
     //Todo change this
-       Glide.with(context).load("https://images-assets.nasa.gov/image/PIA00342/PIA00342~thumb.jpg").into(holder.mImageView);
+        Glide.with(context).load(mImageUrls.get(position)).into(holder.mImageView);
+
     }
 
     @Override
     public int getItemCount() {
 
         //todo change this
-        return 5;
+        return mImageUrls.size();
     }
 
 
